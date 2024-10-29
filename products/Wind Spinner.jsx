fe17a3c4cbe9404425d14c15226034a74f440a4c
Hoanghieutb97 <hoanghieutb97@gmail.com>
2024-10-29 11:26:55 +0700

    sdvs

---
+++ products/Wind Spinner.jsx
@@ -0,0 +1,44 @@
+
+
+//////////////////////////////////////////////////////////////
+#include "createDocument.jsx";
+for (var i = stt; i <= arr.length - 1; i++) {
+    var marginItem = 30;
+    #include "convertPixel.jsx";
+    openFile(FileDesign, arr[i], "0front");
+    #include "cropBlackCut-2Size.jsx";
+    if ((yPosition + boxH + hLast) > hAll && (xPosition + boxW + wLast) > wAll) {
+        app.activeDocument.layerSets["CMYK"].artLayers.getByName(arr[i].stt).remove();
+        app.activeDocument.layerSets["KHUNG"].artLayers.getByName(arr[i].stt).remove();
+        app.activeDocument.layerSets["SPOT"].artLayers.getByName(arr[i].stt).remove();
+
+        #include "saveallcrop.jsx";
+        $.evalFile(File("//192.168.1.240/photoshop-script-V4-ultimate/label/createm-autoFill.jsx")); // in tem
+        #include "createDocument.jsx";
+        ban = ban + 1;
+        openFile(FileDesign, arr[i], "0front");
+        #include "cropBlackCut-2Size.jsx";
+        stt = i;
+    }
+    #include "caculatorPosition.jsx";
+    #include "translateCMYK.jsx";
+    { // translate khung
+        app.activeDocument.activeLayer = app.activeDocument.layerSets["KHUNG"].artLayers.getByName(arr[i].stt);
+        app.doAction("moveZero", "tool");
+        app.activeDocument.activeLayer.translate((xPosition + bounds3[0] - bounds1[0]), (yPosition + bounds1[3] - bounds3[3]) * (-1));
+        app.doAction("lat guong", "tool");  
+    }
+    { // translate khung
+        app.activeDocument.activeLayer = app.activeDocument.layerSets["SPOT"].artLayers.getByName(arr[i].stt);
+        app.doAction("moveZero", "tool");
+        app.activeDocument.activeLayer.translate((xPosition + bounds2[0] - bounds1[0]), (yPosition + bounds1[3] - bounds2[3]) * (-1));
+    }
+    // #include "xoaExpand1px.jsx";
+
+    if (i == arr.length - 1) {
+
+        #include "saveallcrop.jsx";
+        $.evalFile(File("//192.168.1.240/photoshop-script-V4-ultimate/label/createm-autoFill.jsx"));
+    }
+}
+
