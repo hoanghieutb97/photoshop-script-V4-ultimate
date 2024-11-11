73b93d3e422f1fda089e4336c09c16c122c0c5f5
Hoanghieutb97 <hoanghieutb97@gmail.com>
2024-11-11 17:53:51 +0700

    sdvds

--- products/Stained Glass Suncatcher.jsx
+++ products/Stained Glass Suncatcher.jsx
@@ -1,47 +1,62 @@
 
 #include "createDocument.jsx";
+nameSave = "tool";
+
+
 for (var i = stt; i <= arr.length - 1; i++) {
     #include "convertPixel.jsx";
 
-    openFile(FileDesign, arr[i], type);
-    app.doAction("canvasHoriz", "tool");
-    app.activeDocument.activeLayer.name = "1";
-    // app.doAction("path xu ly vien", "tool");
-    // app.doAction("xu ly canh thot", "tool");
-    { // crop xóa khoảng trắng
-        var PSpotKhung = app.activeDocument.activeLayer.bounds;
-        app.activeDocument.crop(PSpotKhung, 0, PSpotKhung[2] - PSpotKhung[0], PSpotKhung[3] - PSpotKhung[1]);
-    }
-    app.activeDocument.resizeImage(UnitValue(wphone, "px"), UnitValue(hphone, "px"), 300, ResampleMethod.BICUBIC);
+    openCropFile(arr[i], FileDesign, "")
 
     if ((yPosition + boxH + hLast) > hAll && (xPosition + boxW + wLast) > wAll) {
         app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
-        
-        #include "save1Mat.jsx";
+
+        #include "saveallcropByName.jsx";
         $.evalFile(File("//192.168.1.240/photoshop-script-V4-ultimate/label/createm-autoFill.jsx")); // in tem
         #include "createDocument.jsx";
-        ban = ban + 1;
-        openFile(FileDesign, arr[i], type);
-        app.doAction("canvasHoriz", "tool");
-        app.activeDocument.activeLayer.name = "1";
-        // app.doAction("path xu ly vien", "tool");
-        // app.doAction("xu ly canh thot", "tool");
-        { // crop xóa khoảng trắng
-            var PSpotKhung = app.activeDocument.activeLayer.bounds;
-            app.activeDocument.crop(PSpotKhung, 0, PSpotKhung[2] - PSpotKhung[0], PSpotKhung[3] - PSpotKhung[1]);
-        }
-        app.activeDocument.resizeImage(UnitValue(wphone, "px"), UnitValue(hphone, "px"), 300, ResampleMethod.BICUBIC);
-        stt = i;
 
+        ban = ban + 1;
+        openCropFile(arr[i], FileDesign, "front")
+        stt = i;
     }
+
     #include "caculatorPosition.jsx";
     app.activeDocument.activeLayer.name = arr[i].stt;
     app.activeDocument.activeLayer.duplicate(app.documents["GLLM"].layerSets["CMYK"], ElementPlacement.PLACEATBEGINNING);
     app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
     #include "translateCMYK.jsx";
+    openKhung(arr[i]);
+    #include "translateKHUNG.jsx";
+
     if (i == arr.length - 1) {
-        #include "save1Mat.jsx";
-        $.evalFile(File("//192.168.1.240/photoshop-script-V4-ultimate/label/createm-autoFill.jsx"));
+        #include "saveallcropByName.jsx";
+        $.evalFile(File("//192.168.1.240/photoshop-script-V4-ultimate/label/createm-autoFill.jsx")); // in tem
+
     }
 }
 
+function openKhung(item) {
+
+    app.open(File("//192.168.1.240/ps script data/Stained Glass Suncatcher/khung " + item.nameId + ".png"));
+    app.activeDocument.activeLayer.name = item.stt;
+    app.activeDocument.activeLayer.duplicate(app.documents["GLLM"].layerSets["KHUNG"], ElementPlacement.PLACEATBEGINNING);// đưa file in sang bên bàn in
+    app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+}
+
+function openCropFile(item, FileDesign, type) {
+    var tenKhung = item.nameId
+    app.open(File("//192.168.1.240/ps script data/Stained Glass Suncatcher/" + item.nameId + ".png"));
+    openFile(FileDesign, item, type);
+    app.doAction("strokeWhite1px", "tool");
+
+
+    #include "cropAndResize-autoFill.jsx";
+    app.activeDocument.activeLayer.name = "1 copy";
+    app.activeDocument.activeLayer.duplicate(app.documents[tenKhung + ".png"], ElementPlacement.PLACEATBEGINNING);// đưa file in sang bên bàn in
+    app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+    app.doAction("moveCenter", "tool");
+    app.doAction("crop mica dzt", "tool");
+    app.doAction("strokeWhite1px", "tool");
+
+
+}
